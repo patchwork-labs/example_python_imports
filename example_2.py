e44c8fe0aecccf5aa4d3333b0c38c686ca882a46
Sebastian Ouslis <sebastian.elo.ouslis@gmail.com>
2023-01-19 09:57:02 -0500

    update for tests

--- example_2.py
+++ example_2.py
@@ -1,4 +1,16 @@
+#from example_1 import how_many_cows
 import example_1
+#from kafka import KafkaConsumer, KafkaProducer
+import kafka.chess as k
+
+consumer = k.KafkaConsumer(
+    'my-topic',
+     bootstrap_servers=['kafka:9092']
+)
+
+greg = k.KafkaProducer(
+    bootstrap_servers=['kafka:9092']
+)
 
 example_1.how_many_cows()
 print("wowza")
