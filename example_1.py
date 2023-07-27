bbec1e3c53878a24f2a1b12cb179e6248123fedc
Sebastian Ouslis <sebastian.elo.ouslis@gmail.com>
2023-07-26 21:05:12 -0400

    add classes with same name functions

--- example_1.py
+++ example_1.py
@@ -3,3 +3,23 @@ from resources import test_python
 def calculate_size():
     return 4
 print("size calculated")
+
+class pants:
+    def __init__(self):
+        self.size = 100
+
+    def string_version(self):
+        return "Pant of size "+str(self.size)
+    
+class shirts:
+    def __init__(self):
+        self.size = 50
+
+    def string_version(self):
+        return "Shirt of size "+str(self.size)
+
+jeans = pants()
+polo = shirts()
+print(jeans.string_version())
+print(polo.string_version())
+
